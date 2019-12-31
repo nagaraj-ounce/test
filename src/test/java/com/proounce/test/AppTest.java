558c62726e11dd7e1259ac9a46889a4a48752d49
nagaraj-ounce <nagaraj.palle@proounce.com>
2019-12-31 13:43:51 +0530

    testing no of tests

--- src/test/java/com/proounce/test/AppTest.java
+++ src/test/java/com/proounce/test/AppTest.java
@@ -40,19 +40,20 @@ public class AppTest
     {
         assertTrue( true );
         System.out.println( "Hello World!" );
-        System.setProperty("webdriver.chrome.driver", "C:\\Testing\\chromedriver_win32\\chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
-		        //open application
-		driver.get("http://localhost:3000/"); 
-		        //login page
-		WebElement var=driver.findElement(By.id("username"));
-		var.sendKeys("SAEVANS");
-		WebElement pass=driver.findElement(By.id("password"));
-		pass.sendKeys("welcome123"); 
-		driver.findElement(By.xpath("//*[@id=\"root\"]/div/main/div[2]/div/form/label/span[1]/span[1]/input")).click();
-		driver.findElement(By.xpath(".//*[@class='MuiButton-label']")).click();
-		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.findElement(By.xpath(".//*[@title='AASF']")).click();
+        
+//        System.setProperty("webdriver.chrome.driver", "C:\\Testing\\chromedriver_win32\\chromedriver.exe");
+//		WebDriver driver=new ChromeDriver();
+//		driver.manage().window().maximize();
+//		        //open application
+//		driver.get("http://localhost:3000/"); 
+//		        //login page
+//		WebElement var=driver.findElement(By.id("username"));
+//		var.sendKeys("SAEVANS");
+//		WebElement pass=driver.findElement(By.id("password"));
+//		pass.sendKeys("welcome123"); 
+//		driver.findElement(By.xpath("//*[@id=\"root\"]/div/main/div[2]/div/form/label/span[1]/span[1]/input")).click();
+//		driver.findElement(By.xpath(".//*[@class='MuiButton-label']")).click();
+//		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//		driver.findElement(By.xpath(".//*[@title='AASF']")).click();
     }
 }
