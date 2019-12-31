46ca7ce62ce476eb62029ee7185fece2dc37ffff
nagaraj-ounce <nagaraj.palle@proounce.com>
2019-12-31 13:33:55 +0530

    hello

--- src/main/java/com/proounce/test/App.java
+++ src/main/java/com/proounce/test/App.java
@@ -1,5 +1,10 @@
 package com.proounce.test;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,19 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.setProperty("webdriver.chrome.driver", "C:\\Testing\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		        //open application
+		driver.get("http://localhost:3000/"); 
+		        //login page
+		WebElement var=driver.findElement(By.id("username"));
+		var.sendKeys("SAEVANS");
+		WebElement pass=driver.findElement(By.id("password"));
+		pass.sendKeys("welcome123"); 
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div/main/div[2]/div/form/label/span[1]/span[1]/input")).click();
+		driver.findElement(By.xpath(".//*[@class='MuiButton-label']")).click();
+		//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath(".//*[@title='AASF']")).click();
     }
 }
